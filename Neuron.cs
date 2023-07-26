12a07f09a44561b7d51992f1a32df279667e69be
Wolex <wolexprag@gmail.com>
2023-07-26 12:51:20 +0300

    Добавьте файлы проекта.

---
+++ Neuron.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Neuron
+{
+    private decimal weight = 0.5m;
+    public decimal LastError { get; private set; }
+    public decimal Smoothing { get; set; } = 0.00001m;
+    public decimal ProcessInputData(decimal input)
+    {
+        return input * weight;
+    }
+    public decimal RestoreInputData(decimal output)
+    {
+        return output / weight;
+    }
+    public void Train(decimal input, decimal expectedResult)
+    {
+        var actualResult = input * weight;
+        LastError = expectedResult - actualResult;
+        var correction = (LastError / actualResult) * Smoothing;
+        weight += correction;
+    }
+    public int Training(decimal input, decimal expectedResult)
+    {
+        int i = 0;
+        do
+        {
+            i++;
+            Train(input, expectedResult);
+        } while (LastError > Smoothing || LastError < -Smoothing);
+        return i;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System;
+
+
+namespace Lear1
+{
+
+    class Program
+    {
+        public static void Print(object text)
+        {
+            Console.WriteLine(text);
+        }
+        public static string Input(string text = "")
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+        public static decimal InputDecimal(string text = "")
+        {
+            Console.Write(text);
+            return decimal.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
