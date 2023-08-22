53bd048ad57603f351948c1e303b68c12bd33f4f
Wolex <wolexprag@gmail.com>
2023-08-22 23:07:43 +0300

    Solve

--- Program.cs
+++ Program.cs
@@ -1,41 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 namespace Lear1
 {
     internal class Program
     {
         public static void Main()
         {
-            List<int> arr = new List<int>() { 3, -1, 1, -3,-5 };
-            Console.WriteLine(Solve(arr));
+            Console.WriteLine(ToCamelCase("The-warion-For"));
          
         }
-        public static int Solve(List<int> arr)
+        public static string ToCamelCase(string str)
         {
-            return arr.Distinct().Sum();
-        }
-        public static bool Cheak1(List<int> chekenList, int numberH)
-        {
-            for(int i = 0; i < chekenList.Count;i++)
+            bool needUp = false;
+            string retstr = "";
+            for (int i = 0; i < str.Length; i++)
             {
-                if (chekenList[i] == numberH)
+                char setr = str[i];
+                if (needUp == true)
                 {
-                    return true;
+                    setr = Char.ToUpper(setr);
+                    needUp = false;
+                }
+                if (setr.Equals('-') || setr.Equals('_'))
+                {
+                    needUp = true;
+                }
+                else
+                {
+                    retstr = retstr + setr.ToString();
                 }
             }
-            return false;
+
+            return retstr;
         }
-        public static bool Cheak2(List<int> chekenList, int numberH)
-        {
-            for (int i = 0; i < chekenList.Count; i++)
-            {
-                if (chekenList[i]*-1 == numberH)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+
     }
 }
