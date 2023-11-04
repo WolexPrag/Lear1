5cadfdaf08d7c568709c7f60bf808d3d22270c76
Wolex <wolexprag@gmail.com>
2023-11-04 16:49:06 +0300

    Trying to implemented MyCoderofk

---
+++ Other/Coderofk/Codering.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn1.Other.Coderofk
+{
+    public static class TestCodering
+    {
+        public static Codering UseCodering()
+        {
+            Console.Write("Enter text:");
+            Codering coder = new Codering(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 0; i < coder.getChars().Count; i++)
+            {
+                Console.Write($"([{i}]'{coder.getChars()[i]}')");
+                if (i + 1 < coder.getChars().Count)
+                {
+                    Console.Write(',');
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(coder.getIndexingText());
+
+            Console.WriteLine(coder.getText());
+            return coder;
+        }
+    }
+    public class Codering
+    {
+        protected List<char> symbols;
+        protected int currentPos;
+        protected string text;
+        public Codering(List<char> symbols, string text, int currentPos = 0)
+        {
+            this.symbols = symbols;
+            this.text = text;
+            this.currentPos = currentPos;
+        }
+        public Codering(string text)
+        {
+            Decode(text);
+            currentPos = 0;
+        }
+        public string getText()
+        {
+            return Encode();
+        }
+        public List<char> getChars()
+        {
+            return symbols;
+        }
+        public string getIndexingText()
+        {
+            return text;
+        }
+        protected void Decode(string text)
+        {
+            List<char> lchar = new List<char>();
+            string codestr = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (lchar.Contains(text[i]) == false)
+                {
+                    lchar.Add(text[i]);
+                }
+                codestr = codestr + ' ' + lchar.IndexOf(text[i]);
+            }
+            symbols = lchar;
+            this.text = codestr;
+        }
+        protected string Encode()
+        {
+            string res = "";
+            while (text.Length > currentPos)
+            {
+                res = res + getSymbol();
+                currentPos += 1;
+            }
+            return res;
+        }
+        protected char Peek(int addPos = 0)
+        {
+            if (currentPos + addPos >= text.Length) return '\0';
+            return text[currentPos + addPos];
+        }
+        protected char getSymbol()
+        {
+            string index = "";
+            for (int i = currentPos; i < text.Length; i++)
+            {
+                currentPos = i;
+                if (index.Equals("") && Peek().Equals(' '))
+                {
+                    continue;
+                }
+                else if (Peek().Equals(' ') || Peek().Equals('\0'))
+                {
+                    break;
+                }
+                index = index + Peek();
+            }
+
+
+            return symbols[Convert.ToInt32(index)];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using Learn1.Other.Coderofk;
 namespace Learn1
 {
     internal class Program
@@ -8,105 +9,8 @@ namespace Learn1
 
         public static void Main(string[] args)
         {
-            Solution sol = new Solution();
-            ListNode l1l1 = new ListNode(2);
-            ListNode l1l2 = new ListNode(4, l1l1);
-            ListNode l1l3 = new ListNode(3, l1l2);
-            ListNode l2l1 = new ListNode(5);
-            ListNode l2l2 = new ListNode(6, l2l1);
-            ListNode l2l3 = new ListNode(4, l2l2);
-            Console.WriteLine(sol.getNumber(sol.getListNode(53)));
-        }
-
-    }
-
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
+            
         }
     }
-
-    public class Solution
-    {
-        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
-        {
-            ListNode currentl1 = l1;
-            ListNode currentl2 = l2;
-            return l1;
-        }
-        public int getNumber(ListNode l1)
-        {
-            int res = 0;
-            int teenPow = 1;
-            List<int> numbers = new List<int>();
-            ListNode current = l1;
-            for (int i = 0; true; i++)
-            {
-                if (current == null)
-                {
-                    break;
-                }
-                numbers.Add(current.val);
-                current = current.next;
-            }
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                res += numbers[i]*teenPow;
-                teenPow = teenPow * 10;
-            }
-            return res;
-        }
-        public ListNode getListNode(int num1)
-        {
-            string needAdd = $"{num1}"; 
-            List<ListNode> nodes = new List<ListNode>(5);
-            for (int i = 0; i<needAdd.Length; i++)
-            {
-                if (i < 2)
-                {
-                    nodes.Add(new ListNode(Parsing(needAdd[i])));
-                    
-                }
-                else
-                {
-                    nodes.Add(new ListNode(Parsing(needAdd[i]), nodes[nodes.Count - 1]));
-                }
-               
-            }
-            return nodes[nodes.Count - 1];
-        }
-        public int Parsing(char num)
-        {
-            switch (num)
-            {
-                case '0':
-                    return 0;
-                case '1':
-                    return 1;
-                case '2':
-                    return 2;
-                case '3':
-                    return 3;
-                case '4':
-                    return 4;
-                case '5':
-                    return 5;
-                case '6':
-                    return 6;
-                case '7':
-                    return 7;
-                case '8':
-                    return 8;
-                case '9':
-                    return 9;
-                default:
-                    return int.MinValue;
-            }
-        }
-    }
+   
 }
