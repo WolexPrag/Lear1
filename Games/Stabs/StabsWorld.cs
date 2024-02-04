e7b51020dbf38a1c72d1d525d02168ad83825144
Wolex <wolexprag@gmail.com>
2024-02-04 19:54:11 +0300

    Test lists method

---
+++ Games/Stabs/StabsWorld.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn1.Games.Stabs
+{
+    public class FloarFight
+    {
+        protected List<PointFight> points;
+    }
+    public class WayFight
+    {
+        protected PointFight _where;
+        public void SendUnit(Unit unit)
+        {
+
+        }
+    }
+    public class PointFight
+    {
+
+        protected List<Unit> units;
+        protected List<WayFight> ways;
+        public Unit? FindSimilarUnit(Unit which)
+        {
+            return units.Find(cheakTarget => cheakTarget.IsSimilar(which) == true);
+        }
+        public void AddUnit(Unit addedUnit)
+        {
+            Unit similar = FindSimilarUnit(addedUnit);
+            if (similar == null)
+            {
+                units.Add(addedUnit);
+                return;
+            }
+            similar.count += addedUnit.count;
+        }
+        public void RemoveUnit(Unit removableObject)
+        {
+
+        }
+    }
+    public class Unit
+    {
+        protected int _count; public int count { get { return _count; } set { _count = value; } }
+        protected Team _team; public Team team { get { return _team; } }
+        public bool IsSimilar(Unit unit)
+        {
+            return this.team == unit.team;
+        }
+    }
+    sealed public class Team
+    {
+        public Team(string name)
+        {
+            this._name = name;
+        }
+        private string _name; public string name
+        { get { return _name; } protected set { _name = value; } }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,65 +1,46 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
+using System.Transactions;
+using System.Collections;
+using System.Numerics;
+using System.Linq;
 
 namespace Learn1
 {
-    public class Node
-    {
-        public Node next;
-        public string name;
-    }
+   
     internal class Program
     {
-        public static string GetData()
-        {
-            char[] simbA = {'a','e','u' };
-            char[] simbB = { 'b','d','z' };
-            string ret = "";
-            Random randomizer = new Random();
-            int sizeData = 5;
-            int countA = 0;
-            for (int i = 0; i < sizeData; i++)
-            {
-                
-                if(countA < 1)
-                {
-                    ret += simbA[randomizer.Next(0, simbB.Length - 1)];
-                    countA++;
-                }
-                else
-                {
-                    ret += simbB[randomizer.Next(0, simbB.Length - 1)];
-                    countA = 0;
-                }
 
-            }
-            return ret;
-        }
-        public static Node Input(int count = 10)
-        {
-            List<Node> ret = new List<Node>(count);
-            for (int i = 0; i < count; i++)
-            {
-                ret.Add(new Node() { name = GetData() }) ;
-                if (i > 0)
-                {
-                    ret[i - 1].next = ret[i];
-                }
-            }
-            return ret[0];
-        }
         public static void Main(string[] args)
         {
-            Node nodes = Input();
-            for (int i = 0;; i++)
+            List<Block> blocks = new List<Block>(10);
+            blocks.Add(new Block() { id = 9});
+            blocks.Add(new Block() { id = 1});
+            blocks.Add(new Block() { id = 3});
+            blocks.Add(new Block() { id = 2});
+            blocks.Add(new Block() { id = 5});
+            blocks.Add(new Block() { id = 1});
+            blocks.Add(new Block() { id = 5});
+            blocks.Add(new Block() { id = 324});
+            blocks.Add(new Block() { id = 0});
+            blocks.Add(new Block() { id = -67});
+            for (int i = 0; i < blocks.Count; i++)
             {
-                Console.WriteLine(nodes.name);
-
-                
-                nodes = nodes?.next;
-
+                Console.WriteLine($"{blocks[i].id}");
             }
             
+            blocks = blocks.GroupBy(x => x.id == 1).Select(x=> x.First()).ToList();
+            Console.WriteLine("======================");
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                Console.WriteLine($"{blocks[i].id}");
+            }
         }
+        
     }
+    public class Block
+    {
+        public int id;
+    }  
 }
